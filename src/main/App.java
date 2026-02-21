78dbde5ca0232ce5c5f86e7549869f57ccdc5704
Bhavana S Gowda <bhavanagowda28@gmail.com>
2026-02-21 19:37:08 +0530

    Auto deployment test version 2

---
+++ src/main/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🚀 Auto Deployment Test - Version 2");
+        System.out.println("Build Time: " + new java.util.Date());
+    }
+}
+
+
+
