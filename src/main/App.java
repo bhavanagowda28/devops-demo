79c9a8324ebd54dfc4bc8fe707110425279a730a
Bhavana S Gowda <bhavanagowda28@gmail.com>
2026-02-21 19:50:53 +0530

    Update version number in deployment test message

--- src/main/App.java
+++ src/main/App.java
@@ -1,7 +1,7 @@
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("🚀 Auto Deployment Test - Version 2");
+        System.out.println("🚀 Auto Deployment Test - Version 28");
         System.out.println("Build Time: " + new java.util.Date());
     }
 }
