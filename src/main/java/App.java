c6c750450b7d3b73305bc4f6ef63d219bd70c87f
Bhavana S Gowda <bhavanagowda28@gmail.com>
2026-02-21 19:42:57 +0530

    Proper App.java update

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🚀 Auto Deployment Test - Correct Location");
+        System.out.println("Deployment Time: " + new java.util.Date());
+    }
+}
+
+
